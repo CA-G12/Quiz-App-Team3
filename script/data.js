5f1ace0aa96358ceded2b0f6ce53c7cecdad84cf
Rabee <ralbhaisi96@gmail.com>
2022-07-21 10:44:52 +0300

    Create main file structure for the project related #1

---
+++ script/data.js
@@ -0,0 +1,385 @@
+const data = [
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question:
+      "Which of the following will write the message “Hello DataFlair!” in an alert box?",
+    answers: [
+      "alertBox(“Hello DataFlair!”)",
+      "alert(Hello DataFlair!);",
+      "msgAlert(“Hello DataFlair!”); ",
+      "alert(“Hello DataFlair!”);",
+    ],
+    correctAns: "alert(“Hello DataFlair!”);",
+  },
+  {
+    question: "How do you find the minimum of x and y using JavaScript?",
+    answers: [" min(x,y);", " Math.min(x,y)", "Math.min(xy)", "min(xy);"],
+    correctAns: "Math.min(x,y)",
+  },
+  {
+    question:
+      "Which JavaScript label catches all the values, except for the ones specified?",
+    answers: ["catch", "label", "try", "default"],
+    correctAns: "default",
+  },
+  {
+    question:
+      "Which are the correct “if” statements to execute certain code if “x” is equal to 2?",
+    answers: ["if(x 2)", "if(x = 2)", "if(x == 2)", " if(x != 2 )"],
+    correctAns: "if(x == 2)",
+  },
+  {
+    question: "What will the code return?\nBoolean(3 < 7)",
+    answers: ["true", "false", "NaN", "SyntaxError"],
+    correctAns: "true",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    answers: ["Client", "Server", "Both", "None"],
+    correctAns: "Both",
+  },
+];
